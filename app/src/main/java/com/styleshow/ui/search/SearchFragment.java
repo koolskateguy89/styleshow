f15ce1f3b6aad4c436f064765f73090b09ae49dc
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-05-08 00:55:51 +0100

    refactor: use material search bar for search fragment (WIP - might move search into home fragment)

--- app/src/main/java/com/styleshow/ui/search/SearchFragment.java
+++ app/src/main/java/com/styleshow/ui/search/SearchFragment.java
@@ -1,6 +1,8 @@
 package com.styleshow.ui.search;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import com.styleshow.adapters.ProfilePreviewAdapter;
 import com.styleshow.databinding.FragmentSearchBinding;
 import dagger.hilt.android.AndroidEntryPoint;
+import timber.log.Timber;
 
 // TODO: handle loading state
 
+/**
+ * @see <a href="https://github.com/material-components/material-components-android/blob/master/docs/components/Search.md#putting-it-all-together">
+ *     Material Search Component</a>
+ */
 @AndroidEntryPoint
 public class SearchFragment extends Fragment {
 
@@ -29,18 +36,35 @@ public class SearchFragment extends Fragment {
 
         binding = FragmentSearchBinding.inflate(inflater, container, false);
 
-        binding.sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        binding.sv.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ignore
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
-                viewModel.setQuery(newText);
-                return false;
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                viewModel.setQuery(s.toString());
             }
         });
+
+        //binding.sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        //    @Override
+        //    public boolean onQueryTextSubmit(String query) {
+        //        return false;
+        //    }
+        //
+        //    @Override
+        //    public boolean onQueryTextChange(String newText) {
+        //        viewModel.setQuery(newText);
+        //        return false;
+        //    }
+        //});
 
         // Setup RecyclerView
         binding.rvProfiles.setLayoutManager(new LinearLayoutManager(getContext()));
