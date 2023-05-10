f0f7eec8d97b48dc161024f3c6361dda03123b9e
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-05-10 01:17:28 +0100

    feat: PostActionsView

---
+++ app/src/main/java/com/styleshow/components/PostActionsView.java
@@ -0,0 +1,88 @@
+package com.styleshow.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import com.styleshow.R;
+import com.styleshow.databinding.ViewPostActionsBinding;
+
+/**
+ * View that contains the actions for a post. Does not contain any logic, just UI.
+ *
+ * <ul>
+ *     <li>Like</li>
+ *     <li>Comment</li>
+ *     <li>Share</li>
+ * </ul>
+ */
+public class PostActionsView extends ConstraintLayout {
+
+    private ViewPostActionsBinding binding;
+    private boolean liked;
+
+    public PostActionsView(@NonNull Context context) {
+        super(context);
+        init(null);
+    }
+
+    public PostActionsView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public PostActionsView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    public PostActionsView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet attrs) {
+        binding = ViewPostActionsBinding.inflate(
+                LayoutInflater.from(getContext()),
+                this,
+                true
+        );
+
+        TypedArray a = getContext().getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.PostActionsView,
+                0, 0);
+
+        try {
+            liked = a.getBoolean(R.styleable.PostActionsView_liked, false);
+        } finally {
+            a.recycle();
+        }
+
+        binding.setLiked(liked);
+    }
+
+    public boolean isLiked() {
+        return liked;
+    }
+
+    public void setLiked(boolean liked) {
+        this.liked = liked;
+        binding.setLiked(liked);
+    }
+
+    public void setOnLikeClickListener(OnClickListener listener) {
+        binding.btnLike.setOnClickListener(listener);
+    }
+
+    public void setOnCommentClickListener(OnClickListener listener) {
+        binding.btnComment.setOnClickListener(listener);
+    }
+
+    public void setOnShareClickListener(OnClickListener listener) {
+        binding.btnShare.setOnClickListener(listener);
+    }
+}
