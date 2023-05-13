a799f8743bcc2ede0f18c8ebf609381cdaa75cee
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-05-13 03:58:53 +0100

    refactor: remove saving last tab, breaks too often

--- app/src/main/java/com/styleshow/ui/MainNavigationActivity.java
+++ app/src/main/java/com/styleshow/ui/MainNavigationActivity.java
@@ -1,8 +1,6 @@
 package com.styleshow.ui;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -23,25 +21,7 @@ public class MainNavigationActivity extends AppCompatActivity {
         binding = ActivityMainNavigationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        SharedPreferences sharedPrefs = getPreferences(MODE_PRIVATE);
-        String lastDestinationKey = getString(R.string.sp_last_destination_key);
-
-        @IdRes int lastDestinationId = sharedPrefs.getInt(lastDestinationKey, R.id.navigation_home);
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_navigation);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-        // Open last tab
-        if (lastDestinationId != R.id.navigation_home)
-            navController.navigate(lastDestinationId);
-
-        // Save opened tab to shared prefs
-        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-            @IdRes int id = destination.getId();
-
-            sharedPrefs.edit()
-                    .putInt(lastDestinationKey, id)
-                    .apply();
-        });
     }
 }
